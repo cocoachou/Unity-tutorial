ee677504ffda4c8d67f4b9ff45c3d01802784551
cocoachou <yujinlee0131@gmail.com>
2023-06-28 18:05:43 +0900

    Add prototype2

---
+++ week2/Create with Code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound) // 현재 오브젝트 위치 > topBound
+        {
+            Destroy(gameObject); // gameObject 제거
+        }
+        else if (transform.position.z < lowerBound) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
